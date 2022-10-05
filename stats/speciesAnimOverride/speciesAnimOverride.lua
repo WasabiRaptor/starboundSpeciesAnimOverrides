a4d9882645b44c6f9713f63cf063fd498720e16d
WasabiRaptor <ketchupraptor@gmail.com>
2022-10-05 19:59:12 -0400

    actually this is better

--- stats/speciesAnimOverride/speciesAnimOverride.lua
+++ stats/speciesAnimOverride/speciesAnimOverride.lua
@@ -827,6 +827,8 @@ function animatedActiveItem(item, itemDescriptor, itemOverrideData, hand, part, 
 					stateData.current, startNew, stateData.updated = table.unpack(setAnimData)
 					if old ~= stateData.current or startNew then
 						stateData.time = -script.updateDt() -- cancel increment to 0
+					end
+					if old ~= stateData.current then
 						refreshImages = true
 					end
 				end
